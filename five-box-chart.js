52f7e5fdbdffd75786d25148022212fb723d2899
Alan <alan.da.silva.ferreira@hotmail.com>
2017-03-15 10:02:52 -0300

    #implement rendering data points

--- five-box-chart.js
+++ five-box-chart.js
@@ -1,18 +1,5 @@
 
-function perByVal(max, val){
-    return (val*100)/max;
-}
-function valByPer(max, per){
-    return ((max * per)/100);
-}
-
-function posByVal(px, maxVal, val){
-    return valByPer(px, perByVal(maxVal, val));
-}
-
-
 function plotFiveBox(options){
-
     var $container = $(options.container);
     var rangeX = options.xAxis.rule.end - options.xAxis.rule.start;
     var rangeY = options.yAxis.rule.end - options.yAxis.rule.start;
@@ -23,84 +10,161 @@ function plotFiveBox(options){
       height: $container.height()
     });
 
-    var mainRect = new Konva.Rect({
-        x: 40, y: 10, width: $container.width() - 100, height: $container.height() - 50, 
-        fill: 'grey', 
-        stroke: 'red'
-    });
-    var mainLayer = new Konva.Layer();
-    
+    var fiveBox = {
+        options: options, 
 
-    /**
-     * retorna as coordenadas x, y em pixels relativo ao mainRect a partir de um valor nas respectivas escalas informadas em xAxis e yAxis
-     * @param {*} x 
-     * @param {*} y 
-     */    
-    function pixelPointByVal(x, y){
-        return {
-                x: posByVal(mainRect.width(), (options.xAxis.rule.end - options.xAxis.rule.start), x), 
-                y: posByVal(mainRect.height(), (options.yAxis.rule.end - options.yAxis.rule.start), y)
-        };
-    }
-    function addLine(x1, y1, x2, y2, stroke, strokeWidth){
-        var p1 = pixelPointByVal(x1, y1);
-        var p2 = pixelPointByVal(x2, y2);
-
-        return mainLayer.add(new Konva.Line({
-            points: [p1.x + mainRect.x(), p1.y + mainRect.y(), p2.x + mainRect.x(), p2.y+ mainRect.y()], 
-            stroke: stroke ? stroke : 'red', 
-            strokeWidth: strokeWidth ? strokeWidth : 1, 
-        }));
-    }
-
-    // draw grid
-    for(var x = options.xAxis.rule.start; x < options.xAxis.rule.end; x+=options.xAxis.rule.step){
-        addLine(x, 0, x, options.yAxis.rule.end, options.grid.stroke.x);
-    }
-
-    for(var y = options.yAxis.rule.start; y < options.yAxis.rule.end + options.yAxis.rule.step; y+=options.yAxis.rule.step){
-        addLine(0, y, options.xAxis.rule.end, y, options.grid.stroke.y);
-    }
-
-    /**
-     * Retorna um Path de um retangulo a partir de pontos de escala
-     * @param {Number} vx posicao inicial horizontal do retangulo na escala horizontal
-     * @param {Number} vy posicao inicial vertical do retangulo na escala horizontal
-     * @param {Number} vw largura do retangulo na escala horizontal
-     * @param {Number} vh altura do retangulo na escala horizontal
-     */
-    function rectByValue(vx, vy, vw, vh, fill, stroke, strokeWidth){
-        var rangePoint = pixelPointByVal(rangeX, rangeY);
+        $container: $container, 
+        stage: stage, 
+        mainRect: {}, 
+        mainLayer: {}, 
         
-        var p = pixelPointByVal(vx, vy);
-        var s = pixelPointByVal(vw, vh);
-        var layer = new Konva.Layer();
+        perByVal: function (max, val){
+            return (val*100)/max;
+        },
+        valByPer: function (max, per){
+            return ((max * per)/100);
+        },
+        posByVal: function (px, maxVal, val){
+            return this.valByPer(px, this.perByVal(maxVal, val));
+        }, 
+        /**
+         * retorna as coordenadas x, y em pixels relativo ao mainRect a partir de um valor nas respectivas escalas informadas em xAxis e yAxis
+         * @param {*} x 
+         * @param {*} y 
+         */    
+        pixelPointByVal: function (x, y){
+            return {
+                    x: this.posByVal(this.mainRect.width(), (options.xAxis.rule.end - options.xAxis.rule.start), x), 
+                    y: this.posByVal(this.mainRect.height(), (options.yAxis.rule.end - options.yAxis.rule.start), y)
+            };
+        }, 
+        addLine: function (x1, y1, x2, y2, stroke, strokeWidth){
+            var p1 = this.pixelPointByVal(x1, y1);
+            var p2 = this.pixelPointByVal(x2, y2);
 
-        var rect = new Konva.Rect({
-                x: mainRect.x() + p.x, 
-                //y: (rangePoint.y - (s.y - p.y)) + mainRect.y(),
-                y:  mainRect.y() + ((rangePoint.y - s.y)),
-                width: s.x - p.x, 
-                height: (s.y - p.y), 
-                fill: (fill ? fill : 'grey'), 
-                stroke: (stroke ? stroke : null), 
-                strokeWidth: (strokeWidth ? strokeWidth : null)
-        });
-        layer.add(rect);
-        stage.add(layer);
+            var line = new Konva.Line({
+                points: [p1.x + this.mainRect.x(), p1.y + this.mainRect.y(), p2.x + this.mainRect.x(), p2.y+ this.mainRect.y()], 
+                stroke: stroke ? stroke : 'red', 
+                strokeWidth: strokeWidth ? strokeWidth : 1, 
+            });
+            
+            this.mainLayer.add(line);
+            return line;
+        }, 
+        addPoint: function (x, y, radius, fill, stroke, strokeWidth){
+            var p = this.pixelPointByVal(x, y);
 
-        return layer;
+            var point = new Konva.Circle({
+                x: p.x + this.mainRect.x(),
+                y: p.y + this.mainRect.y(),
+                radius: radius ? radius : 2,
+                fill: fill ? fill : 'black',
+                stroke: stroke ? stroke : 'red', 
+                strokeWidth: strokeWidth ? strokeWidth : 1, 
+            });
+            
+            this.mainLayer.add(point);
+            return point;
+        }, 
+        /**
+         * Retorna um Path de um retangulo a partir de pontos de escala
+         * @param {Number} vx posicao inicial horizontal do retangulo na escala horizontal
+         * @param {Number} vy posicao inicial vertical do retangulo na escala horizontal
+         * @param {Number} vw largura do retangulo na escala horizontal
+         * @param {Number} vh altura do retangulo na escala horizontal
+         */
+        rectByValue: function (vx, vy, vw, vh, fill, stroke, strokeWidth){
+            var rangePoint = this.pixelPointByVal(rangeX, rangeY);
+            
+            var p = this.pixelPointByVal(vx, vy);
+            var s = this.pixelPointByVal(vw, vh);
+
+            var rect = new Konva.Rect({
+                    x: this.mainRect.x() + p.x, 
+                    y:  this.mainRect.y() + ((rangePoint.y - s.y)),
+                    width: s.x - p.x, 
+                    height: (s.y - p.y), 
+                    fill: (fill ? fill : 'grey'), 
+                    stroke: (stroke ? stroke : null), 
+                    strokeWidth: (strokeWidth ? strokeWidth : null)
+            });
+
+            this.mainLayer.add(rect);
+
+            return rect;
+        },
+        clear: function(){
+            
+            this.stage.clear();
+            this.stage.clearCache();
+            this.stage.children.clear();
+            this.stage.children.removeChildren();
+            //this.stage.removeAll();
+            
+            this.mainLayer = new Konva.Layer();
+
+            this.mainRect = new Konva.Rect({
+                x: 40, y: 10, width: $container.width() - 100, height: $container.height() - 50, 
+                fill: 'grey', 
+                stroke: 'red'
+            });
+            this.stage.add(this.mainLayer);
+
+        },
+        update: function(){
+            this.clear();
+
+            for(var b = 0; b < options.boxes.length; b++){
+                var box = options.boxes[b];
+                var rect = this.rectByValue(box.x, box.y, box.w, box.h, box.fill);
+                
+                var title = new Konva.Text({
+                    text: box.title,
+                    fontSize: 30,
+                    fontFamily: 'Calibri',
+                    fill: 'green'
+                });
+
+                title.x(rect.x()+((rect.width()/2)-(title.textWidth/2)));
+                title.y(rect.y()+((rect.height()/2)-(title.textHeight/2)));
+                this.mainLayer.add(title);
+            }
+
+            // draw grid
+            for(var x = options.xAxis.rule.start; x < options.xAxis.rule.end; x+=options.xAxis.rule.step){
+                this.addLine(x, 0, x, options.yAxis.rule.end, options.grid.stroke.x);
+            }
+
+            for(var y = options.yAxis.rule.start; y < options.yAxis.rule.end + options.yAxis.rule.step; y+=options.yAxis.rule.step){
+                this.addLine(0, y, options.xAxis.rule.end, y, options.grid.stroke.y);
+            }
+
+            for(var d = 0; d < options.data.length; d++){
+                var dataItem = options.data[d];
+                
+                this.addPoint(dataItem.x, dataItem.y, dataItem.radius);
+            }
+
+            this.stage.add(this.mainLayer);
+
+            this.stage.draw();
+
+        }
     }
 
 
-    for(var b = 0; b < options.boxes.length; b++){
-        var box = options.boxes[b];
-        rectByValue(box.x, box.y, box.w, box.h, box.fill);
-    }
+    $container.resize(function(){
+        fiveBox.stage.width($container.width());
+        fiveBox.stage.height($container.height());
+
+        fiveBox.update();
+    });
+
+    fiveBox.update();
+
+    return fiveBox;
 
 
-
-    stage.add(mainLayer);
 
 }
 
